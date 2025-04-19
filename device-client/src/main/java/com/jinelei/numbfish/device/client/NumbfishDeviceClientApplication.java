5d5aadb33ff528e9852f064df5417a6ab1bb246d
jinelei <jinelei@163.com>
2025-04-19 18:04:19 +0800

    引入SpringHelper

--- device-client/src/main/java/com/jinelei/numbfish/device/client/NumbfishDeviceClientApplication.java
+++ device-client/src/main/java/com/jinelei/numbfish/device/client/NumbfishDeviceClientApplication.java
@@ -1,16 +1,20 @@
 package com.jinelei.numbfish.device.client;
 
 import com.jinelei.numbfish.acquisition.configuration.annotation.EnableInfluxService;
+import com.jinelei.numbfish.common.helper.SpringHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+@SuppressWarnings("unused")
+@Import(SpringHelper.class)
 @EnableInfluxService
 @SpringBootApplication(scanBasePackageClasses = {NumbfishDeviceClientApplication.class})
 public class NumbfishDeviceClientApplication {
