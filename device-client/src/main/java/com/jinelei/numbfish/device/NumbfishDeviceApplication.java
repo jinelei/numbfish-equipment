0ace176030ad37e950d482406aff6e28c99516ab
jinelei <jinelei@163.com>
2025-04-25 17:14:45 +0800

    类重命名

---
+++ device-client/src/main/java/com/jinelei/numbfish/device/NumbfishDeviceApplication.java
@@ -0,0 +1,22 @@
+package com.jinelei.numbfish.device;
+
+import com.jinelei.numbfish.acquisition.configuration.annotation.EnableInfluxService;
+import com.jinelei.numbfish.common.helper.SpringHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SuppressWarnings("unused")
+@Import(SpringHelper.class)
+@EnableInfluxService
+@SpringBootApplication(scanBasePackageClasses = {NumbfishDeviceApplication.class})
+public class NumbfishDeviceApplication {
+    private static final Logger log = LoggerFactory.getLogger(NumbfishDeviceApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbfishDeviceApplication.class, args);
+    }
+
+}
