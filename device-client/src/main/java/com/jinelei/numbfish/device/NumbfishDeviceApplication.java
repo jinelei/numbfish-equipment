518d3ae50ea97aeaccb4a5600a6fe46345df31a3
jinelei <jinelei@gmail.com>
2025-04-27 22:00:54 +0800

    移除auth扫描

--- device-client/src/main/java/com/jinelei/numbfish/device/NumbfishDeviceApplication.java
+++ device-client/src/main/java/com/jinelei/numbfish/device/NumbfishDeviceApplication.java
@@ -6,13 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @SuppressWarnings("unused")
-@Import(SpringHelper.class)
+@Import({SpringHelper.class})
 @EnableInfluxService
-@SpringBootApplication(scanBasePackageClasses = {NumbfishDeviceApplication.class}, exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(scanBasePackageClasses = {NumbfishDeviceApplication.class})
 public class NumbfishDeviceApplication {
     private static final Logger log = LoggerFactory.getLogger(NumbfishDeviceApplication.class);
 
